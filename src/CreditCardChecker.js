dc181d5cd1189f9c0135c0e2e137eb42b5855f5c
sirtau <contact@hamidmousa.com>
2021-08-21 11:09:57 +1000

    Updated CC Checker Component
    Reduced the number of variables tracked in state from 2 to 1.
    No longer storing the result of checkBank function, passing the function directly to insert which bank the number is associated with via returning a string.

--- src/CreditCardChecker.js
+++ src/CreditCardChecker.js
@@ -6,43 +6,38 @@ class CreditCardChecker extends React.Component {
         this.state = {
             cardNumber: ''
         }
-        this.handleBankChange = this.handleBankChange.bind(this)
         this.checkBank = this.checkBank.bind(this)
-        this.updateBank = this.updateBank.bind(this)
+        this.updateCard = this.updateCard.bind(this)
 
     }
 
-    handleBankChange(CCInput) {
-        const newNumber = CCInput
-        this.setState({ cardNumber: newNumber })
+    checkBank() {
+        const testNumber = this.state.cardNumber
         
-    }
-
-    updateBank(event) {
-        this.checkBank(event.target.value)
-    }
-
-    checkBank(ccNumber) {
-        if (/(^34)|(^37)/.test(ccNumber)) {
-            this.handleBankChange('AMEX')
-        } else if (/^6001/.test(ccNumber)) {
-            this.handleBankChange('Discover')
-        } else if (/^51|^52|^53|^54|^55/.test(ccNumber)) {
-            this.handleBankChange('MasterCard')
-        } else if (/^4/.test(ccNumber)) {
-            this.handleBankChange('Visa')
+        if (/^3[47]/.test(testNumber)) {
+            return 'AMEX'
+        } else if (/^6001/.test(testNumber)) {
+            return 'Discover'
+        } else if (/^5[1-5]/.test(testNumber)) {
+            return 'MasterCard'
+        } else if (/^4/.test(testNumber)) {
+            return 'Visa'
         } else {
-            this.handleBankChange('##')
+            return '##'
         }
     } 
+
+    updateCard(event) {
+        this.setState({ cardNumber: event.target.value })
+    }
     
     render() {
         return (
             <div className="ccChecker">
                 <h5>Credit Card Bank Checker:</h5>
                 
-                <input type="number" onChange={this.updateBank} />
-                <span> <p>Bank: {this.state.cardNumber}</p></span> 
+                <input type="number" onChange={this.updateCard} />
+                <span> <p>Bank: {this.checkBank()}</p></span> 
             </div>
         )
     }
